75453403ff61cac3ff5cc28b9de9bb3ab2eb2565
Chris Inch <github@chrisinch.com>
2016-03-30 22:09:10 -0400

    Added a config file for Twitter creds.

---
+++ config-sample.rb
@@ -0,0 +1,8 @@
+
+APP_NAME = 'melting-pot'
+
+# Create your app at https://apps.twitter.com/ and retrieve the following info
+CONSUMER_KEY = 'abc123'
+CONSUMER_SECRET = 'abc123'
+ACCESS_TOKEN = 'abc123'
+ACCESS_TOKEN_SECRET = 'abc123'
--- melting-pot.rb
+++ melting-pot.rb
@@ -3,12 +3,24 @@ require 'rubygems'
 # adds the Twitter module - http://www.rubydoc.info/gems/twitter/Twitter
 require 'twitter'
 
+config_file = './config.rb'
+
+require config_file if File.file? config_file
+
+APP_NAME ||= ''
+CONSUMER_KEY ||= ''
+CONSUMER_SECRET ||= ''
+ACCESS_TOKEN ||= ''
+ACCESS_TOKEN_SECRET ||= ''
+
+puts APP_NAME
+
 # do not commit real config data to git.
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
+  config.consumer_key        = CONSUMER_KEY
+  config.consumer_secret     = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_TOKEN_SECRET
 end
 
 client.mentions_timeline.each do |tweet|
